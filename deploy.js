931e7b8799caded42b9c5e98910095a2c891a14c
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-08 22:04:48 +0100

    Add deploy contract code

--- deploy.js
+++ deploy.js
@@ -1,6 +1,31 @@
-// Compile them separately
+// Compile separately
 
-async function main() {}
+// Import ethers js
+const ethers = require("ethers");
+const fs = require("fs");
+
+async function main() {
+  // Ganache Test Blockchain: http://127.0.0.1:8545
+  // Link script to the local blockchain
+  const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
+
+  // Connect a ganache account
+  const wallet = new ethers.Wallet(
+    "0x12fa2277b4bb3a36dffea553ecddb59fcb1f057c7061b07bff2e5ffb894d0fa3",
+    provider
+  );
+
+  // Take ABI and binary code in order to interact with the contract
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
+
+  // Create an object used to deploy contract
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying...");
+  // Deploy contract
+  const contract = await contractFactory.deploy();
+  console.log(contract);
+}
 
 main()
   .then(() => process.exit(0))
